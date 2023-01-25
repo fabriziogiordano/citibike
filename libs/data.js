03fcef58eaf4c3d3fd39db1d1077cc234454f6bb
Fabrizio Giordano <giordano.fabrizio@gmail.com>
2023-01-25 04:15:16 +0000

    initial

---
+++ libs/data.js
@@ -0,0 +1,93 @@
+const myStations = [
+  {
+    label: "Home",
+    name: "Pearl St & Hanover Square",
+    order: 0,
+  },
+  {
+    label: "Work",
+    name: "W 33 St & 10 Ave",
+    order: 1,
+  },
+  {
+    label: "Work2",
+    name: "W 34 St & Hudson Blvd E",
+    order: 2,
+  },
+  {
+    label: "Subway",
+    name: "W 31 St & 7 Ave",
+    order: 3,
+  },
+];
+
+async function findStations() {
+  const url = "https://gbfs.citibikenyc.com/gbfs/en/station_information.json";
+  let data = await fetch(url);
+
+  if (data.status === 200) {
+    let {
+      data: { stations },
+    } = await data.json();
+
+    const myStationNames = myStations.map(({ name }) => name);
+    stations = stations.map(station => {
+      if (myStationNames.includes(station.name)) {
+        const mine = myStations.find(s => s.name === station.name);
+        return {
+          ...station,
+          label: mine.label,
+          order: mine.order,
+        }
+      }
+
+      return {}
+    });
+
+    return stations;
+  }
+}
+
+export async function findAvailability() {
+  const myStations = await findStations();
+
+  const url = "https://gbfs.citibikenyc.com/gbfs/en/station_status.json";
+  let data = await fetch(url);
+
+  if (data.status === 200) {
+    let {
+      data: { stations },
+    } = await data.json();
+
+    const myStationId = myStations.map(({ station_id }) => station_id);
+
+    const myStationsFiltered = [];
+    stations = stations.map((station) => {
+      // Filter only the stations I care about
+      if (myStationId.includes(station.station_id)) {
+        const myStation = myStations.find(({ station_id }) => station_id === station.station_id);
+        myStationsFiltered.push({
+          raw: {
+            station,
+            myStation
+          },
+          station_id: station.station_id,
+          label: myStation.label,
+          order: myStation.order,
+          monster: 'true',
+          name: myStation.name,
+          num_bikes_availabxle: station.num_bikes_available,
+          num_ebikes_available: station.num_ebikes_available,
+          num_docks_available: station.num_docks_available,
+          lat: myStation.lat,
+          lon: myStation.lon,
+        });
+      }
+    });
+
+    myStationsFiltered.sort((orderFirst, orderSecond) => orderFirst.order - orderSecond.order);
+    // console.log(JSON.stringify(myStationsFiltered, null, 2));
+
+    return myStationsFiltered;
+  }
+}
---
+++ server.js
@@ -0,0 +1,41 @@
+import Fastify from "fastify";
+import fastifyView from "@fastify/view";
+
+import path from "path";
+import * as url from "url";
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+import { findAvailability } from "./libs/data.js";
+
+const app = Fastify({
+  logger: false,
+});
+
+import { Liquid } from "liquidjs";
+const engine = new Liquid({
+  root: path.join(__dirname, "templates"),
+  extname: ".liquid",
+});
+
+app.register(fastifyView, {
+  engine: {
+    liquid: engine,
+  },
+});
+
+// Declare a route
+app.get("/", async (request, reply) => {
+  const data = await findAvailability();
+  return reply.view("./templates/index.liquid", {data});
+});
+
+// Run the server!
+const start = async () => {
+  try {
+    await app.listen({ port: 4000 });
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+start();
